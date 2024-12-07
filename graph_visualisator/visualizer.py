83dd86bcc2ffd8d353177c6b2a6114578af6bf65
Guardian1221 <asmalov2017@mail.ru>
2024-12-07 16:51:58 +0300

    upd: fix errors

--- graph_visualisator/visualizer.py
+++ graph_visualisator/visualizer.py
@@ -4,6 +4,8 @@ import sys
 import json
 
 class DependencyVisualizer:
+
+    
     def __init__(self, config_file):
         self.config = configparser.ConfigParser()
         self.config.read(config_file)
