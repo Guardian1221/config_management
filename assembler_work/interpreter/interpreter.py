a35c9b135a78137f573849e2dbe8bd990e1fbc77
Guardian1221 <164686269+Guardian1221@users.noreply.github.com>
2025-11-11 17:46:27 +0300

    Update interpreter.py

--- assembler_work/interpreter/interpreter.py
+++ assembler_work/interpreter/interpreter.py
@@ -7,16 +7,13 @@ def interpret(binary_file, result_file, mem_start, mem_end):
     with open(binary_file, 'rb') as bin_file:
         memory = [0] * 100  # Инициализация памяти (100 ячеек)
 
-        # Чтение бинарного файла и интерпретация
         while True:
-            instruction = bin_file.read(7)  # Чтение 7 байт на инструкцию
+            instruction = bin_file.read(7)
             if len(instruction) < 7:
-                break  # Если данных меньше 7 байт, выходим из цикла
+                break
 
-            # Распаковка инструкции
             opcode, arg1, arg2 = struct.unpack('<IBH', instruction)
 
-            # Выполнение действий в зависимости от opcode
             if opcode == (26 << 3) | 0x1:  # LOAD
                 memory[arg1] = arg2
             elif opcode == (26 << 3) | 0x2:  # READ
@@ -38,11 +35,9 @@ def interpret(binary_file, result_file, mem_start, mem_end):
         tree.write(result_file)
 
 if __name__ == "__main__":
-    # Взятие аргументов из командной строки
     binary_file = sys.argv[1]
     result_file = sys.argv[2]
     mem_start = int(sys.argv[3])
     mem_end = int(sys.argv[4])
 
-    # Запуск интерпретатора
     interpret(binary_file, result_file, mem_start, mem_end)
